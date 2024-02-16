81c4595019cb76c9930c70eedaaeb0c7956d2113
Andrea De Felice <andrea.defelice@ko2.it>
2024-02-16 11:45:44 +0100

    metrics

--- plotter.py
+++ plotter.py
@@ -6,20 +6,47 @@ import argparse
 def plot_csv(filename, filename2):
     # Read the CSV file
     data = pd.read_csv(filename)
+    data2 = None
+    position2 = None
+    objective2 = None
+
+    if filename2:
+        data2 = pd.read_csv(filename2)
+
+    # Find min length of the two files
+    min_length = len(data)
+    if filename2:
+        min_length = min(min_length, len(data2))
+
+    # Truncate the data to the min length
+    data = data[:min_length]
+    if filename2:
+        data2 = data2[:min_length]
 
     # Use the index for the x-axis and the first three columns for the y-axis
     x = data.index
-    y1 = data.iloc[:, 0]
-    y2 = data.iloc[:, 1]
+    objective = data.iloc[:, 0]
+    position_1 = data.iloc[:, 1]
+
+    if filename2:
+        objective2 = data2.iloc[:, 0]
+        position2 = data2.iloc[:, 1]
 
     # Create the plot
-    plt.plot(x, y1, label="Objective")
-    plt.plot(x, y2, label=filename)
-    plt.plot(x, y1 - y2, label="Difference")
+    plt.plot(x, objective, label="Objective")
+    plt.plot(x, position_1, label=filename)
+    plt.plot(x, objective - position_1, label="Difference")
 
-    # ignore first 10% and last 10% of the data
-    objective_short = y1[int(len(y1) * 0.1) : int(len(y1) * 0.9)]
-    filename_short = y2[int(len(y2) * 0.1) : int(len(y2) * 0.9)]
+    if filename2:
+        plt.plot(x, objective2, label="Objective2")
+        plt.plot(x, position2, label=filename2)
+        plt.plot(x, objective2 - position2, label="Difference2")
+
+    # ignore first 5% and last 5% of the data
+    objective_short = objective[int(len(objective) * 0.05) : int(len(objective) * 0.95)]
+    filename_short = position_1[
+        int(len(position_1) * 0.05) : int(len(position_1) * 0.95)
+    ]
 
     print(f"Total error of {filename} is: ", sum(abs(filename_short - objective_short)))
     print(
@@ -34,25 +61,26 @@ def plot_csv(filename, filename2):
     )
 
     if filename2:
-        data2 = pd.read_csv(filename2)
-        y2 = data2.iloc[:, 1]
-        plt.plot(x, y2, label=filename2)
-
-        filename_short = y2[int(len(y2) * 0.1) : int(len(y2) * 0.9)]
+        filename2_short = position2[
+            int(len(position2) * 0.05) : int(len(position2) * 0.95)
+        ]
+        objective2_short = objective2[
+            int(len(objective2) * 0.05) : int(len(objective2) * 0.95)
+        ]
 
         print(
             f"Total error of {filename2} is: ",
-            sum(abs(filename_short - objective_short)),
+            sum(abs(filename2_short - objective2_short)),
         )
         print(
             f"The maximum error of {filename2} is: ",
-            max(abs(filename_short - objective_short)),
+            max(abs(filename2_short - objective2_short)),
         )
         print(
             "Mean error of ",
             filename2,
             " is: ",
-            sum(abs(filename_short - objective_short)) / len(filename_short),
+            sum(abs(filename2_short - objective2_short)) / len(filename2_short),
         )
 
     # Add a legend
